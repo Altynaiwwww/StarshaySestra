a61e33a15a4e36d976e7fe67396473567a5092a9
Altynaiwwww <121087662+Altynaiwwww@users.noreply.github.com>
2023-06-02 00:25:02 +0300

    Кнопка назад

---
+++ StarshaySestra/History.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bots.Types;
+
+namespace StarshaySestra
+{
+    public static class History
+    {
+        public static List<HistoryItem> histories;
+
+        public static void AddAction(HistoryItem histories)
+        {
+            histories.Add(histories);
+        }
+
+        public static void RemoveLastAction()
+        {
+            if (histories.Count > 0)
+            {
+                histories.RemoveAt(histories.Count - 1);
+
+            }
+        }
+
+
+      
+    }
+
+  
+}
---
+++ StarshaySestra/HistoryItem.cs
@@ -0,0 +1,47 @@
+﻿
+using Telegram.Bot.Types.ReplyMarkups;
+
+
+namespace StarshaySestra
+{
+    public class HistoryItem
+    {
+        public List<ReplyKeyboardMarkup> KeyboardButtons;
+
+    
+        List<HistoryItem> userActions = new List<HistoryItem>();
+        
+        public void AddButton(HistoryItem histories)
+        {
+            userActions.Add(histories);
+        }
+
+        public void AddButtonWithAction(HistoryItem histories)
+        {
+            userActions.Add(histories);
+            histories.AddAction(userActions);
+        }
+
+        private void AddAction(List<HistoryItem> userActions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveLastButton(HistoryItem histories)
+        {
+            if (userActions.Count > 0)
+            {
+                userActions.RemoveAt(userActions.Count - 1);
+                
+            }
+        }
+
+        internal void Add(HistoryItem histories)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+    }
+}
