2cba8b3ee33853a1122e5b88b6750505e09e0165
Altynaiwwww <121087662+Altynaiwwww@users.noreply.github.com>
2023-06-07 23:01:02 +0300

    Im done

--- StarshaySestra/ButtonResponses.cs
+++ StarshaySestra/ButtonResponses.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bots;
 using Telegram.Bots.Http;
 
 namespace StarshaySestra
@@ -19,10 +20,6 @@ namespace StarshaySestra
                           new KeyboardButton[]
                           {
                            new KeyboardButton("Психолог"),
-                          },
-                          new KeyboardButton[]
-                          {
-                           new KeyboardButton("Адвокат"),
                           },
                           new KeyboardButton[]
                           {
@@ -53,5 +50,136 @@ namespace StarshaySestra
             await botClient.SendTextMessageAsync(chatId: chatId, text: TextConstants.MainDisclaimer,
               replyMarkup: SOS);
         }
+
+
+        public static async Task ShowButtonParents(ITelegramBotClient botClient, ChatId chatId) 
+        {
+            var parents = new ReplyKeyboardMarkup(new KeyboardButton[][]
+                           {
+                            new KeyboardButton[]
+                            {
+                             new KeyboardButton("Cложные отношения с родителями"),
+                            },
+                            new KeyboardButton[]
+                            {
+                             new KeyboardButton("Родители и развод"),
+                            },
+                            new KeyboardButton[]
+                            {
+                            new KeyboardButton("Назад")
+                            }
+
+
+                           });
+            await botClient.SendTextMessageAsync(chatId:chatId, text: TextConstants.ParentsRelationships,
+              replyMarkup: parents);
+        }
+
+        public static async Task ShowInstagramResponse(ITelegramBotClient botClient, ChatId chatId) 
+        {
+            var socials = new ReplyKeyboardMarkup(new KeyboardButton[][]
+                           {
+                            new KeyboardButton[]
+                            {
+                              new KeyboardButton("Безопасность в сети"),
+                            },
+                            new KeyboardButton []
+                            {
+                              new KeyboardButton("Инста как фильтр"),
+                            },
+                            new KeyboardButton []
+                            {
+                              new KeyboardButton("Назад")
+                            }
+
+                           });
+            await botClient.SendTextMessageAsync(chatId: chatId, text:TextConstants.Insagram,
+             replyMarkup: socials);
+
+        }
+        public static async Task ShowSexResponse(ITelegramBotClient botClient, ChatId chatId) 
+        {
+            var sex = new ReplyKeyboardMarkup(new KeyboardButton[][]
+                         {
+                           new KeyboardButton[]
+                           {
+                             new KeyboardButton("Методы защиты")
+                           },
+                           new KeyboardButton[]
+                           {
+                             new KeyboardButton("Половое образование"),
+                           },
+                           new KeyboardButton[]
+                           {
+                             new KeyboardButton("Месячные"),
+                           },
+                           new KeyboardButton[]
+                           {
+                             new KeyboardButton("Назад")
+                           }
+
+
+                         });
+            await botClient.SendTextMessageAsync(chatId: chatId, text: TextConstants.Seks,
+                       replyMarkup: sex);
+            
+        }
+
+        public static async Task ShowAbuseReponse(ITelegramBotClient botClient, ChatId chatId) 
+        {
+            var abuse = new ReplyKeyboardMarkup(new KeyboardButton[][]
+    {
+                            new KeyboardButton[]
+                            {
+                            new KeyboardButton("Виды насилие"),
+                            },
+                            new KeyboardButton[]
+                            {
+                             new KeyboardButton("Душевное состояние"),
+                            },
+                            new KeyboardButton[]
+                            {
+                            new KeyboardButton("Назад")
+                            }
+    });
+            await botClient.SendTextMessageAsync(chatId: chatId, text:TextConstants.Seks, 
+          replyMarkup: abuse);
+
+        }
+
+
+        public static async Task ShowButtonBack(ITelegramBotClient botClient, ChatId chatId) 
+        {
+            var mainMenu = new ReplyKeyboardMarkup(
+                              new[]
+                              {
+                                           new[]
+                                           {
+                                              new KeyboardButton("SOS"),
+                                           },
+                                           new[]
+                                           {
+                                               new KeyboardButton("Секс"),
+                                           },
+                                           new[]
+                                           {
+                                            new KeyboardButton("Как распознать арбузера?")
+                                           },
+                                           new[]
+                                           {
+                                           new KeyboardButton("Соц.сети и массовое информация ")
+                                           },
+                                           new[]
+                                           {
+                                            new KeyboardButton("Отношения с родителями "),
+                                           }
+                              });
+
+            await botClient.SendTextMessageAsync(chatId:chatId, text: "Вы вернулись в главное меню",
+          replyMarkup: mainMenu);
+        }
+
+       
+
     }
 }
