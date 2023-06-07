eaa6da6a9237c0addf3de1fe76b1e7a1b66de431
Altynaiwwww <121087662+Altynaiwwww@users.noreply.github.com>
2023-06-07 21:33:51 +0300

    for you
    for you

---
+++ StarshaySestra/ButtonResponses.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bots.Http;
+
+namespace StarshaySestra
+{
+    public static class ButtonResponses
+    {
+        public static async Task ShowSOSResponse(ITelegramBotClient botClient, ChatId chatId)
+        {
+            var SOS = new ReplyKeyboardMarkup(new KeyboardButton[][]
+                        {
+                          new KeyboardButton[]
+                          {
+                           new KeyboardButton("Психолог"),
+                          },
+                          new KeyboardButton[]
+                          {
+                           new KeyboardButton("Адвокат"),
+                          },
+                          new KeyboardButton[]
+                          {
+                          new KeyboardButton("Юрист"),
+                          },
+                          new KeyboardButton[]
+                          {
+                            new KeyboardButton("Гинеколог")
+                          },
+                          new KeyboardButton[]
+                          {
+                            new KeyboardButton("Шелтер(это предоставление убежища)"),
+                          },
+                          new KeyboardButton[]
+                          {
+                            new KeyboardButton("Терапевт")
+                          },
+                          new KeyboardButton[]
+                          {
+                            new KeyboardButton("Телефон доверия:")
+                          },
+                          new KeyboardButton[]
+                          {
+                            new KeyboardButton("Назад")
+                          }
+                        });
+
+            await botClient.SendTextMessageAsync(chatId: chatId, text: TextConstants.MainDisclaimer,
+              replyMarkup: SOS);
+        }
+    }
+}
